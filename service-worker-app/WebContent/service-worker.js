e591efe05ea309ed06aa1f24f00d7897d617d244
James Kranig <jmkranig@yahoo.com>
2018-02-04 15:03:51 -0500

    Added network unavailable page. Updated manifest.json. Added fetch event handling to service-worker.js

--- service-worker-app/WebContent/service-worker.js
+++ service-worker-app/WebContent/service-worker.js
@@ -1,6 +1,55 @@
 
+const cacheName = 'cache_v1';
+const networkUnavailbleUrl = '/service-worker-app/network-unavailable.html';
 
-// Handle push event
+// Install
+self.addEventListener('install', function(event){
+	
+	event.waitUntil(caches.open(cacheName).then(function(cache){ 
+		
+		return cache.addAll([
+			networkUnavailbleUrl,
+			'/service-worker-app/index.html',
+			'/service-worker-app/pages/home.html',
+			'/service-worker-app/images/icon-144x144.png',
+			'/service-worker-app/images/icon-192x192.png'
+		]);
+		
+	}).catch(function(error){
+		
+		console.info('Error installing service worker: ' + error);		
+	}));
+	
+});
+
+
+
+// Fetch 
+self.addEventListener('fetch', function(event) {
+		
+	event.respondWith(caches.match(event.request).then(function(response){
+		
+		return response || fetch(event.request);
+		
+	}).catch(function(event) {
+		
+		// If there is an error fetching from network, display 
+		// "No Network Connection" page (cached)
+		return caches.match(networkUnavailbleUrl);
+    }));
+});
+
+
+
+// Activate 
+self.addEventListener('activate', function(event) {
+	
+	// No cleanup (outdated cache, etc.) to perform.
+});
+
+
+
+// Push event
 self.addEventListener('push', function(event) {
   
 	if (event.data) {
@@ -19,9 +68,3 @@ self.addEventListener('push', function(event) {
 });
 
 
-
-self.addEventListener('fetch', function(e) {
-	
-	// All requests handled by browser.
-	return;
-});
